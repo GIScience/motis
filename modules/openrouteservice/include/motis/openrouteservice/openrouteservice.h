288b73b898ceba4b093a97eae38268aba417ec65
Julian Psotta <julianpsotta@gmail.com>
2024-04-29 15:47:49 +0200

    WIP: Many to many

--- modules/openrouteservice/include/motis/openrouteservice/openrouteservice.h
+++ modules/openrouteservice/include/motis/openrouteservice/openrouteservice.h
@@ -9,6 +9,24 @@ public:
   openrouteservice();
   ~openrouteservice() override;
 
+//  openrouteservice(openrouteservice const&) = delete;
+//  openrouteservice& operator=(openrouteservice const&) = delete;
+//
+//  openrouteservice(openrouteservice&&) = delete;
+//  openrouteservice& operator=(openrouteservice&&) = delete;
+//
+  motis::module::msg_ptr table(motis::module::msg_ptr const&) const;
+
+
+  // create a struct that holds the url and api key
+  struct impl {
+    explicit impl(std::string const& url, std::string const& api_key)
+        : url_(url), api_key_(api_key) {}
+    std::string url_;
+    std::string api_key_;
+  };
+  std::unique_ptr<impl> config;
+
   void init(motis::module::registry&) override;
 
 private:
@@ -16,6 +34,10 @@ private:
   //std::unique_ptr<impl> impl_;
 
   std::string url_;
+  std::string api_key_;
+
+  std::vector<std::string> profiles_;
+  std::map<std::string, std::unique_ptr<router>> routers_;
 };
 
 }  // namespace motis::openrouteservice
--- modules/openrouteservice/src/openrouteservice.cc
+++ modules/openrouteservice/src/openrouteservice.cc
@@ -1,25 +1,34 @@
 #include "motis/openrouteservice/openrouteservice.h"
 
+#include <cista/hash.h>
 #include <filesystem>
 
 #include "motis/core/common/logging.h"
 #include "motis/module/event_collector.h"
+#include "motis/openrouteservice/error.h"
+#include "motis/openrouteservice/router.h"
 
-using namespace motis::module;
-using namespace motis::logging;
+namespace mm = motis::module;
+namespace fs = std::filesystem;
+namespace o = motis::openrouteservice;
+
+namespace mm = motis::module;
 
 namespace motis::openrouteservice {
 
-openrouteservice::openrouteservice(): module("Openrouteservice Options", "openrouteservice") {
+openrouteservice::openrouteservice()
+    : module("Openrouteservice Options", "openrouteservice") {
   param(url_, "url", "ORS API endpoint");
 }
 
 openrouteservice::~openrouteservice() = default;
 
 void openrouteservice::init(motis::module::registry& reg) {
-  std::cout << "Servus ORS!";
-  /*
-  reg.subscribe("/init", [this] { init_async(); }, {});
+  std::cout << "URL: " << this->url_ << std::endl;
+  std::cout << "API key: " << this->api_key_ << std::endl;
+  // Store the url and api key in the impl struct
+  config = std::make_unique<impl>(url_, api_key_);
+  //  reg.subscribe("/init", [this] { init_async(); }, {});
   reg.register_op("/osrm/table",
                   [this](msg_ptr const& msg) {
                     auto const req = motis_content(OSRMManyToManyRequest, msg);
@@ -48,4 +57,60 @@ void openrouteservice::init(motis::module::registry& reg) {
                   */
 }
 
-}  // namespace motis::osrm
+std::string_view translate_mode(std::string_view s) {
+  switch (cista::hash(s)) {
+    case cista::hash("foot"): return "foot-walking";
+    case cista::hash("bike"): return "cycling-regular";
+    case cista::hash("car"): return "driving-car";
+    default: return "foot-walking";
+  }
+}
+
+template <typename Req>
+mm::msg_ptr sources_to_targets(Req const* req, openrouteservice::impl* config) {
+  auto const timer = motis::logging::scoped_timer{"openrouteservice.matrix"};
+//  auto& thor = config->get();
+//
+//  // Encode OSRMManyToManyRequest as valhalla request.
+//  auto doc = j::Document{};
+//  encode_request(req, doc);
+//
+//  // Decode request.
+//  v::Api request;
+//  v::from_json(doc, v::Options::sources_to_targets, request);
+//  auto& options = *request.mutable_options();
+//
+//  // Get the costing method.
+//  auto mode = v::sif::TravelMode::kMaxTravelMode;
+//  auto const mode_costing = thor.factory_.CreateModeCosting(options, mode);
+//
+//  // Find path locations (loki) for sources and targets.
+//  thor.loki_worker_.matrix(request);
+//
+//  // Run matrix algorithm.
+//  auto const res = thor.matrix_.SourceToTarget(
+//      options.sources(), options.targets(), *config->reader_, mode_costing,
+//      mode, 4000000.0F);
+//  thor.matrix_.clear();
+//
+//  // Encode OSRM response.
+  mm::message_creator fbb;
+//  fbb.create_and_finish(
+//      MsgContent_OSRMOneToManyResponse,
+//      CreateOSRMOneToManyResponse(fbb, fbb.CreateVectorOfStructs(utl::to_vec(
+//                                           res,
+//                                           [](v::thor::TimeDistance const& td) {
+//                                             return motis::osrm::Cost{
+//                                                 td.time / 60.0, 1.0 * td.dist};
+//                                           })))
+//          .Union());
+  return make_msg(fbb);
+}
+
+mm::msg_ptr openrouteservice::table(mm::msg_ptr const& msg) const {
+  using osrm::OSRMManyToManyRequest;
+  auto const req = motis_content(OSRMManyToManyRequest, msg);
+  return sources_to_targets(req, config.get());
+}
+
+}  // namespace motis::openrouteservice
